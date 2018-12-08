3558f380e8916c136dec01184af2035f53a66314
akhileshtripathi <akhileshtripathi@akhileshtripathi-ThinkPad-T450>
2018-12-08 09:06:31 +0530

    update bubblesort algo

---
+++ src/sort/Bubblesort.java
@@ -0,0 +1,43 @@
+package sort;
+
+public class Bubblesort {
+
+	public static void main(String[] args) {
+		int[] intArray = { 25, 34, -16, 7, 22, 2, -23 };
+
+		// We will start index from last unsorted index because at that point we can
+		// start sorting
+		for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+			// To bubble largest unsorted array element, we will have to start with inner
+			// loop
+			for (int i = 0; i < lastUnsortedIndex; i++) {
+				// If value of i > value of i+1. we will do sorting i.e.
+				if (intArray[i] > intArray[i + 1]) {
+					swap(intArray, i, i + 1);
+				}
+
+			}
+
+		}
+		// Lets print the value of sorted array
+		for (int i = 0; i < intArray.length; i++) {
+
+			System.out.println(intArray[i]);
+		}
+
+	}
+
+	// Algorithm to swap elements
+	public static void swap(int[] array, int i, int j) {
+
+		if (i == j) {
+			return;
+		}
+
+		int temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+
+	}
+
+}
